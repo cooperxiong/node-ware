8824b58c0c917110bd34b8371c36fea8559dba0e
Cooper <xiongjiapeng@wecash.net>
2017-09-21 15:39:23 +0800

    improve,add some features

--- index.js
+++ index.js
@@ -11,17 +11,30 @@ class Ware {
         this._middlewares = [];
     }
 
-    _work(middlewares, input = {}, output = {}) {
+    _work(input = {}, output = {}, i = 0) {
         const self = this;
         return co(function* () {
-            if (middlewares.length === 0) return [input, output];
-            const next = middlewares.shift();
-            if (next(input, output).constructor.name === 'Promise') {
-                [input, output] = yield next(input, output)
-            } else {
-                [input, output] = next(input, output);
+            if (self._middlewares.length === i) {
+                self._middlewares = self._middlewares.filter(e => e.name); // remove anonymous functions
+                return [input, output]
             }
-            return self._work(middlewares, input, output);
+            const next = self._middlewares[i](input, output);
+            let next_output, _input, _output;
+            if (next && typeof next.then === 'function') {
+                next_output = yield next;
+            } else {
+                next_output = next
+            }
+            if (!next_output || !next_output.length) {
+                [_input, _output] = [,]
+            } else if (next_output.length === 1) {
+                [_input, _output] = [next_output,]
+            } else if (next_output.length === 2) {
+                [_input, _output] = next_output
+            } else {
+                throw new Error('There is no more than two return values')
+            }
+            return self._work(Object.assign(input, _input || {}), Object.assign(output, _output || {}), i + 1);
         })
     }
 
@@ -48,7 +61,7 @@ class Ware {
     }
 
     run(...args) {
-        return this._work(this._middlewares, args[0], args[1])
+        return this._work(args[0], args[1])
     }
 }
 
